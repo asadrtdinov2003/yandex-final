f1d3086ec18aba62fb0cbf904908ab22fe64ec6c
Rusandor <ruslanpalkin44@gmail.com>
2022-12-10 15:05:15 +0300

    Airbnb config

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const Header = () => {
-  return (
-    <div>
-
-    </div>
-  );
-};
+function Header() {
+  return <div />;
+}
 
 export default Header;
--- src/pages/TeamsPage/TeamsPage.jsx
+++ src/pages/TeamsPage/TeamsPage.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const TeamsPage = () => {
-  return (
-    <div>
-      Команды
-    </div>
-  );
-};
+function TeamsPage() {
+  return <div>Команды</div>;
+}
 
 export default TeamsPage;

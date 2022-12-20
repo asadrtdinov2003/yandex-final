77d23d61a824bfc67faf4944076517af51e87c63
Rusandor <ruslanpalkin44@gmail.com>
2022-12-21 00:12:22 +0300

    feat/themes: сделаны стили для главной страницы

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -11,7 +11,7 @@ function Toggle() {
 
   return (
     <div className={styles.toggle}>
-      <input className={styles.toggle__input} type="checkbox" onClick={handleThemeSwitch}/>
+      <input className={styles.toggle__input} type="checkbox" onClick={handleThemeSwitch} checked={darkMode}/>
       <span className={styles.toggle__label}>
         <div className={styles.toggle__check} />
       </span>
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,6 +4,7 @@ import Reviews from "../../components/Reviews/Reviews";
 import Accordion from "../../components/Accordion/Accordion";
 import styles from "./styles.module.css";
 import { ThemeContext } from "../../Contexts/ThemeContext";
+import darkPcImage from "../../imgs/dark_pc.png";
 
 function MainPage() {
   const { darkMode } = useContext(ThemeContext);
@@ -17,17 +18,17 @@ function MainPage() {
             Не следует, однако забывать, что дальнейшее развитие различных форм деятельности влечет за собой процесс
             внедрения и модернизации новых предложений.
           </p>
-          <button className={cn(styles.button, styles.button_red, styles.mainSection__button)} type="button">
+          <button className={cn(styles.button, darkMode ? styles.button_dark : styles.button_red, styles.mainSection__button)} type="button">
             Связаться с нами
           </button>
         </div>
-        <div className={styles.mainSection__img}>
-          <img src="computer.png" alt="Ноутбук" />
+        <div className={cn(styles.mainSection__img, {[styles.mainSection__img_no_hat]: darkMode})}>
+          <img src={darkMode ? darkPcImage : "computer.png"} alt="Ноутбук" />
         </div>
       </section>
       <section className={styles.section}>
-        <h2 className={cn(styles.title, styles.title_upper)}>О нас</h2>
-        <p className={styles.text}>
+        <h2 className={cn(styles.title, styles.title_upper, {[styles.heading_dark]: darkMode})}>О нас</h2>
+        <p className={cn(styles.text, {[styles.paragraph_dark]: darkMode})}>
           Равным образом дальнейшее развитие различных форм деятельности требуют от нас анализа позиций, занимаемых
           участниками в отношении поставленных задач. Не следует, однако забывать, что рамки и место обучения кадров
           играет важную роль в формировании новых предложений. Идейные соображения высшего порядка, а также сложившаяся
@@ -36,11 +37,11 @@ function MainPage() {
         </p>
       </section>
       <section className={styles.section}>
-        <h2 className={cn(styles.title, styles.title_upper)}>Отзывы</h2>
+        <h2 className={cn(styles.title, styles.title_upper, {[styles.heading_dark]: darkMode})}>Отзывы</h2>
         <Reviews />
       </section>
       <section className={styles.section}>
-        <h2 className={cn(styles.title, styles.title_upper)}>FAQ</h2>
+        <h2 className={cn(styles.title, styles.title_upper, {[styles.heading_dark]: darkMode})}>FAQ</h2>
         <Accordion />
       </section>
     </main>

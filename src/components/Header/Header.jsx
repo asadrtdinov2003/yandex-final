053c1ce67586d2342f3fa16adb5ccf8ab2cc095a
Rusandor <ruslanpalkin44@gmail.com>
2022-12-21 00:42:25 +0300

    fix/themes: теперь нет пустого пространства вокруг страницы.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import classnames from 'classnames';
+import cn from 'classnames';
 import { NavLink } from 'react-router-dom';
 import styles from './styles.module.css';
 import Lightrope from '../Lightrope/Lightrope';
@@ -12,25 +12,25 @@ function Header() {
 
   return (
     <>
-      <header className={styles.header}>
-        <NavLink to="/" className={classnames(styles.logo, styles.tab)}>
+      <header className={cn(styles.header, {[styles.header_dark]: darkMode})}>
+        <NavLink to="/" className={cn(styles.logo, styles.tab, {[styles.text_dark]: darkMode})}>
           WEB
         </NavLink>
-        <nav className={classnames(styles.nav, active ? styles.active : '')}>
+        <nav className={cn(styles.nav, active ? styles.active : '')}>
           <ul className={styles.nav__body}>
-            <NavLink to="teams" className={classnames(styles.nav__item, styles.tab, styles.tab_green)}>
+            <NavLink to="teams" className={cn(styles.nav__item, styles.tab, styles.tab_green)}>
               Наша команда
             </NavLink>
-            <NavLink to="projects" className={classnames(styles.nav__item, styles.tab, styles.tab_green)}>
+            <NavLink to="projects" className={cn(styles.nav__item, styles.tab, styles.tab_green)}>
               Проекты
             </NavLink>
-            <li className={classnames(styles.nav__item, styles.tab, styles.tab_green)}>Связаться с нами</li>
+            <li className={cn(styles.nav__item, styles.tab, styles.tab_green)}>Связаться с нами</li>
           </ul>
         </nav>
         <div className={styles.flex}>
           <Toggle />
           <button type="button" className={styles.burger} onClick={() => setActive((active + 1) % 2)}>
-            <div className={classnames(styles.burger__body, active ? styles.checked : '')}>
+            <div className={cn(styles.burger__body, active ? styles.checked : '')}>
               <div className={styles.burger__item} />
               <div className={styles.burger__item} />
               <div className={styles.burger__item} />
--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -11,7 +11,7 @@ function Toggle() {
 
   return (
     <div className={styles.toggle}>
-      <input className={styles.toggle__input} type="checkbox" onClick={handleThemeSwitch} checked={darkMode}/>
+      <input className={styles.toggle__input} type="checkbox" onChange={handleThemeSwitch} checked={darkMode}/>
       <span className={styles.toggle__label}>
         <div className={styles.toggle__check} />
       </span>

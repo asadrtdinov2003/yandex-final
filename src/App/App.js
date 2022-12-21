128f44a0b99c8d4938fd66b3bfe417d174da7b62
keinerex <Yurkov22Vl@gmail.com>
2022-12-21 23:58:37 +0700

    условный рендеринг кнопки

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { store } from "../store";
 import Layout from "../components/Layout/Layout";
 import TeamsPage from "../pages/TeamsPage/TeamsPage";
 import ProjectsPage from "../pages/ProjectsPage/ProjectsPage";
@@ -9,21 +7,19 @@ import MainPage from "../pages/MainPage/MainPage";
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            {/* Я без понятия какая страница показывается сначала так что
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* Я без понятия какая страница показывается сначала так что
                 TODO: Исправить если неверно
              */}
-            <Route index element={<MainPage />} />
-            <Route path="projects" element={<ProjectsPage />} />
-            <Route path="teams" element={<TeamsPage />} />
-            <Route path="*" element={<div>404</div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+          <Route index element={<MainPage />} />
+          <Route path="projects" element={<ProjectsPage />} />
+          <Route path="teams" element={<TeamsPage />} />
+          <Route path="*" element={<div>404</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/OurMap/OurMap.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { points } from "../../constants/points";
+import {
+  Clusterer,
+  FullscreenControl,
+  Map,
+  ObjectManager,
+  Placemark,
+  YMaps,
+  ZoomControl,
+} from "@pbe/react-yandex-maps";
+import style from "./styles.module.css";
+
+const defaultState = {
+  center: [51.501386, 81.201791],
+  zoom: 3,
+};
+
+function OurMap() {
+  return (
+    <div className={style.map_wrap}>
+      <YMaps>
+        <Map
+          defaultState={defaultState}
+          width={"100%"}
+          height={"100%"}
+          modules={["geoObject.addon.balloon", "geoObject.addon.hint"]}
+        >
+          <FullscreenControl />
+          <ZoomControl options={{ float: "right" }} />
+          <Clusterer
+            options={{
+              preset: "islands#invertedVioletClusterIcons",
+              groupByCoordinates: false,
+            }}
+          >
+            {points.map((point) => (
+              <Placemark
+                key={point.id}
+                geometry={point.coords}
+                options={{
+                  iconColor: point.color, // цвет иконки, можно также задавать в hex
+                }}
+                properties={{
+                  // пару символов помещается
+                  hintContent: `<b>${point.hint}</b>`,
+                  // создаём пустой элемент с заданными размерами
+                  balloonContent: `<div><h2>${point.title}</h2><h3>${point.description}</h3></div>`,
+                  modules: ["geoObject.addon.balloon", "geoObject.addon.hint"],
+                }}
+              />
+            ))}
+          </Clusterer>
+          <ObjectManager
+            options={{
+              clusterize: true,
+              gridSize: 32,
+            }}
+            objects={{
+              openBalloonOnClick: true,
+              preset: "islands#greenDotIcon",
+            }}
+            filter={(object) => object.id % 2 === 0}
+            modules={["objectManager.addon.objectsBalloon", "objectManager.addon.objectsHint"]}
+          />
+        </Map>
+      </YMaps>
+    </div>
+  );
+}
+
+export default OurMap;

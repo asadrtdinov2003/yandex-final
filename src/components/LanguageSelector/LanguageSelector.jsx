bf9a6db474358093b2474b919184ebcd9fa3d574
Rusandor <ruslanpalkin44@gmail.com>
2022-12-22 03:29:51 +0300

    feat/languages: LanguageSelector.jsx (потом подправите стили я спать)

--- src/components/LanguageSelector/LanguageSelector.jsx
+++ src/components/LanguageSelector/LanguageSelector.jsx
@@ -1,15 +1,15 @@
 import React, { useContext } from "react";
 import styles from "./LanguageSelector.module.css";
 import { LanguageContext } from "../../Contexts/LanguageContext";
+import cn from 'classnames';
 
 const LanguageSelector = () => {
   const {lang, setLang, translation} = useContext(LanguageContext);
 
   return (
     <div className={styles.languageSelector}>
-      {lang}
-      <button className={styles.languageSelector__btn} onClick={() => {setLang('ru')}}>RU</button>/
-      <button className={styles.languageSelector__btn} onClick={() => {setLang('en')}}>EN</button>
+      <button className={cn(styles.languageSelector__btn, {[styles.languageSelector__btn_selected]: lang === 'ru'})} onClick={() => {setLang('ru')}}>RU</button>/
+      <button className={cn(styles.languageSelector__btn, {[styles.languageSelector__btn_selected]: lang === 'en'})} onClick={() => {setLang('en')}}>EN</button>
     </div>
   );
 };
